c73eb7e1214d5aabefbb20f3a23926bff08f3f7d
uzaheen <81876961+uzaheen@users.noreply.github.com>
2021-11-30 23:33:50 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,26 @@
+void main(){
+  // var lst = ["ali", "muhmmad", "usman", "15"];
+  // print("lst");
+
+
+  // var lst2 = ["ali", 1.5, true, 65];
+  // print(lst2);
+
+  // var edu = ['SSC', 'HSC', 'BS', 'BCOM', 'MS', 'M.PHIL', 'PHD'];
+  // print(edu);
+
+  // var student = [ 45, 55, 65, 70 ];
+  // student.sort();
+  // print(student);
+
+  // var student = [ 45, 55, 65, 70 ];
+  // student.sort();
+  // print(student.toList());
+
+  List lst1 = [ "ali", "khan", "muhmmad", "javeed"];
+  List lst2 = [ 300, 200, 150, 130];
+  print("${lst1[0]}\t ${lst2[0]} \t ${(lst2[0]/500*100)}");
+  print("${lst1[1]}\t ${lst2[1]} \t ${(lst2[1]/500*100)}");
+  print("${lst1[2]}\t ${lst2[2]} \t ${(lst2[2]/500*100)}");
+  print("${lst1[3]}\t ${lst2[3]} \t ${(lst2[3]/500*100)}");
+}
